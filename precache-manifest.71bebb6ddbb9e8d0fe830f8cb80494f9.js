8b75ff1cbb639e447fe42db1b81bb7697993d0c7
viishal <viishalgohil193@gmail.com>
2022-12-05 11:44:46 +0530

    Updates

---
+++ precache-manifest.71bebb6ddbb9e8d0fe830f8cb80494f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f072011f3d7a315168ab",
+    "url": "/dutils.git/static/css/main.3553c245.chunk.css"
+  },
+  {
+    "revision": "f072011f3d7a315168ab",
+    "url": "/dutils.git/static/js/main.f072011f.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/dutils.git/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "463ce7db04cb925eb082",
+    "url": "/dutils.git/static/js/2.463ce7db.chunk.js"
+  },
+  {
+    "revision": "87635a9f233553ce2012",
+    "url": "/dutils.git/static/js/runtime~main.87635a9f.js"
+  },
+  {
+    "revision": "77c2f52ba47a2b82af1f3e3c0e84bafe",
+    "url": "/dutils.git/index.html"
+  }
+];
